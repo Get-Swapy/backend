b0f074eef2fbaa11e5c267b0e14fb2f190c575e7
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2025-03-17 10:39:09 -0600

    refactor: remove unused methods from UserController

--- canisters/rest-api/src/user/user.controller.ts
+++ canisters/rest-api/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserService } from './user.service';
@@ -6,11 +6,6 @@ import { UserService } from './user.service';
 @Controller('/users')
 export class UserController {
   constructor(private readonly user: UserService) {}
-
-  @Get('/')
-  public getAll() {
-    return this.user.getAll();
-  }
 
   @Post('/')
   public createUser(@Body() data: CreateUserDto) {
@@ -25,10 +20,5 @@ export class UserController {
   @Get('/external/:externalId')
   public getUserByExternalId(@Param('externalId') externalId: string) {
     return this.user.getByExternalId(externalId);
-  }
-
-  @Delete('/:userId')
-  public removeUser(@Param('userId') userId: string) {
-    return this.user.removeById(userId);
   }
 }
